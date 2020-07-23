925eaf08d242e3103df38ebb8c4f55da39dcf39d
siddhartha-pvt <68007058+siddhartha-pvt@users.noreply.github.com>
2020-07-23 16:48:54 +0530

    Update index.js

--- api/routes/esCustomerSearch/index.js
+++ api/routes/esCustomerSearch/index.js
@@ -4,24 +4,17 @@ const multer = require("multer")();
 const data = require("./index.json");
 
 router.post("/", multer.none(), function (req, res, next) {
-	const request = JSON.parse(req.body.data);
-	
-	const customerName = request.customerName;
-	const pageNumber = request.pageNumber;
-	const pageSize = request.pageSize;
-
-	const startingValue = pageNumber * pageSize;
-	const endingValue = (pageNumber + 1) * pageSize;
-
-	const filteredData = data.customer.filter((eachCustomer) =>
-		eachCustomer.customerName
-			.toLowerCase()
-			.includes(customerName.toLowerCase())
-	);
-	res.send({
-		total: filteredData.length,
-		customer: filteredData.slice(startingValue, endingValue),
-	});
+    const { customerName } = JSON.parse(req.body.data);
+    const filteredData = data.customer.filter((eachCustomer) =>
+        eachCustomer.customerName
+            .toLowerCase()
+            .includes(customerName.toLowerCase())
+    );
+    res.send({
+        total: filteredData.length,
+        customer: filteredData,
+        bucketNames: data.bucketNames,
+    });
 });
 
 module.exports = router;
